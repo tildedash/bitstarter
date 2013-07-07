c640ef502528c1d3063138a7d31c5b4f46e65a09
tildedash <julien.malherbe@applidget.com>
2013-07-07 23:37:18 +0200

    read from index.html

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var fileContent = fs.readFileSync('index.html');
+  var content = new Buffer(fileContent, 'utf-8');
+  response.send(content.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
